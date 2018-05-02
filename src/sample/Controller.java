5e9e2a24c9ab65dadb2abe6bf75b29baa6cc3ee7
Li Ronning <li.ronning@gmail.com>
2018-05-02 13:39:10 +0200

    test2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
-package sample;
+package iMat;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import se.chalmers.cse.dat216.project.CartEvent;
+import se.chalmers.cse.dat216.project.ShoppingCartListener;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, ShoppingCartListener {
+
+    // Account pane actions
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    // Shope pane methods
+    @Override
+    public void shoppingCartChanged(CartEvent evt) {
+        // TODO
+    }
 }
