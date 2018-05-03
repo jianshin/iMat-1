cb0ca12b16e2595c9b53a055f508ab0cb5f2abe1
Li Ronning <li.ronning@gmail.com>
2018-05-03 09:52:51 +0200

    added fxml names for parts in thumbnail

---
+++ src/iMat/ItemThumbnail.java
@@ -0,0 +1,24 @@
+package iMat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ItemThumbnail {
+
+    @FXML
+    private ImageView thumbnailImageView;
+    @FXML
+    private Label butThumbnailLabel;
+    @FXML
+    private ImageView addThumbnailImageView;
+    @FXML
+    private ImageView removeThumbnailImageView;
+    @FXML
+    private Label quantityThumbnailLabel;
+    @FXML
+    private Label priceThumbnailLabel;
+    @FXML
+    private Label nameThumbnailLabel;
+
+}
